2312f2d0e2d1c60e7fe8d4a36915e5a1ea193d49
james <ziranjameshe@gmail.com>
2024-01-06 21:45:22 +0000

    002

--- 002_insertion_sort/c++/algo.cpp
+++ 002_insertion_sort/c++/algo.cpp
@@ -1,46 +1,40 @@
 // Insertion Sort
 
-#include <iostream>
 #include <stdio.h>
-#include "../../utility/utility.cpp"
+
+#include <iostream>
+
+#include "../../utility/utility.hpp"
 
 /*
-    1. Time  Complexity - The time  complexity of Insertion Sort is O(N^2) because there are two nested for loops
-    2. Space Complexity - The space complexity of Insertion Sort is O(0)   because there is no extra variable being used during sorting
+1. Time  Complexity - The time  complexity of Insertion Sort is O(N^2) because
+there are two nested for loops
+2. Space Complexity - The space complexity of Insertion Sort is O(0) because
+there is no extra variable being used during sorting
 */
 
+auto constexpr array_size = 10;
+std::array<int, array_size> array_to_sort_mine = {};
+std::array<int, array_size> array_to_sort_official = {};
 
-void insertion_sort(int array_to_sort[], int array_size) {
-
+auto insertion_sort(std::array<int, array_size> &array_to_sort) -> void {
     for (int i = 1; i < array_size; i++) {
-
         for (int j = i; j > 0; j--) {
-
             if (array_to_sort[j - 1] > array_to_sort[j]) {
-
                 std::swap(array_to_sort[j - 1], array_to_sort[j]);
 
             } else {
-
                 break;
             }
         }
     }
 }
 
-int main() {
+auto main() -> int {
+    fill_arrays_with_random_values(array_to_sort_mine, array_to_sort_official);
 
-    const int array_size                   = 10;
-    // int array_to_sort_mine[array_size]     = {25, 64, 333, 12, 22, 11};
-    // int array_to_sort_official[array_size] = {25, 64, 333, 12, 22, 11};
+    insertion_sort(array_to_sort_mine);
+    std::sort(array_to_sort_official.begin(), array_to_sort_official.end());
 
-    int array_to_sort_mine[array_size];
-    int array_to_sort_official[array_size];
-
-    fill_arrays_with_random_values(array_to_sort_mine, array_to_sort_official, array_size);
-
-    insertion_sort(array_to_sort_mine, array_size);
-    std::sort(array_to_sort_official, array_to_sort_official + array_size);
-
-    compare_array(array_to_sort_mine, array_to_sort_official, array_size);
+    compare_array(array_to_sort_mine, array_to_sort_official);
 }
