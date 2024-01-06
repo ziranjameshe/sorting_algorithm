0002b20f8cf859bbffd3656571845c1de889b87b
james <ziranjameshe@gmail.com>
2024-01-06 21:48:37 +0000

    003

--- 003_quick_sort/c++/algo.cpp
+++ 003_quick_sort/c++/algo.cpp
@@ -1,26 +1,29 @@
 // Quick Sort
 
-#include <iostream>
 #include <stdio.h>
-#include "../../utility/utility.cpp"
+
+#include <iostream>
+
+#include "../../utility/utility.hpp"
 
 /*
-    1. Time  Complexity - The time  complexity of Quick Sort is:
-                                                                O(N * log(N)) for average case and best case
-                                                                O(N^2)        for worst case
-    2. Space Complexity - The space complexity of Quick Sort is O(1) because there is no extra variable being used during sorting
+1. Time  Complexity - The time  complexity of Quick Sort is: O(N * log(N)) for
+average case and best case O(N^2) for worst case
+2. Space Complexity - The space complexity of Quick Sort is O(1) because there
+is no extra variable being used during sorting
 */
 
-int partition(int array_to_sort[], int low, int high) {
+auto constexpr array_size = 31;
+std::array<int, array_size> array_to_sort_mine = {};
+std::array<int, array_size> array_to_sort_official = {};
 
+int partition(std::array<int, array_size> &array_to_sort, int low, int high) {
     int pivot = array_to_sort[high];
 
     int i = low;
 
-    for (int j = low; j < high; j++) {
-
+    for (size_t j = low; j < high; j++) {
         if (array_to_sort[j] < pivot) {
-
             std::swap(array_to_sort[i], array_to_sort[j]);
             i++;
         }
@@ -29,34 +32,22 @@ int partition(int array_to_sort[], int low, int high) {
     std::swap(array_to_sort[i], array_to_sort[high]);
 
     return i;
-
 }
 
-void quick_sort(int array_to_sort[], int low, int high) {
-
+void quick_sort(std::array<int, array_size> &array_to_sort, int low, int high) {
     if (low < high) {
-
         int partition_index = partition(array_to_sort, low, high);
 
         quick_sort(array_to_sort, low, partition_index - 1);
         quick_sort(array_to_sort, partition_index + 1, high);
     }
-
 }
 
-int main() {
-
-    const int array_size                   = 31;
-    // int array_to_sort_mine[array_size]     = {25, 64, 333, 12, 22, 11};
-    // int array_to_sort_official[array_size] = {25, 64, 333, 12, 22, 11};
-
-    int array_to_sort_mine[array_size];
-    int array_to_sort_official[array_size];
-
-    fill_arrays_with_random_values(array_to_sort_mine, array_to_sort_official, array_size, 1000);
+auto main() -> int {
+    fill_arrays_with_random_values(array_to_sort_mine, array_to_sort_official);
 
     quick_sort(array_to_sort_mine, 0, array_size - 1);
-    std::sort(array_to_sort_official, array_to_sort_official + array_size);
+    std::sort(array_to_sort_official.begin(), array_to_sort_official.end());
 
-    compare_array(array_to_sort_mine, array_to_sort_official, array_size);
+    compare_array(array_to_sort_mine, array_to_sort_official);
 }
